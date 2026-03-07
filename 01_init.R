e2b8cac27151bf07d9f8454630faae6a0c0eecfa
flaviadangelo <flavia.dangelo@student.univaq.it>
2026-03-07 15:23:43 +0100

    caricamento token

--- 01_init.R
+++ 01_init.R
@@ -2,13 +2,5 @@
 #load packages
   library(learnr)
 
- # --- 1) Imposta l'email usata da Git per i commit (metti la TUA email) ---
- system('git config --global user.email "flavia.dangelo@student.univaq.it"')
- 
- # --- 2) Imposta il nome usato da Git per i commit (metti il TUO nome e cognome) ---
- system('git config --global user.name "flaviadangelo"')
- 
- # --- 3) Controlla che Git abbia salvato le impostazioni globali ---
- # (stampa la lista delle configurazioni globali; cerca user.name e user.email)
- system("git config --global --list")
- 
+ library(usethis)
+ use_git_config(user.name = "flaviadangelo", user.email = "flavia.dangelo@student.univaq.it")
