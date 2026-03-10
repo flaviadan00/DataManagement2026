3e535195d15de43c9b28b3e6cedd96539951c0a4
flaviadangelo <flavia.dangelo@student.univaq.it>
2026-03-10 18:55:41 +0100

    collegamento con github

--- fork.R
+++ fork.R
@@ -1,1 +1,12 @@
+
 system("git remote add upstream https://github.com/micdimu/DataManCons.git")
+
+system("git config --global --list")
+
+system("git remote -v")
+
+system("git fetch upstream")
+
+system("git merge upstream/main")
+
+system("git status")
