9154e7fd8f9c6efab61764197868f5f21e690a0b
flaviadangelo <flavia.dangelo@student.univaq.it>
2026-03-10 14:49:09 +0100

    altra linea

--- 01_init.R
+++ 01_init.R
@@ -4,3 +4,13 @@
 
  library(usethis)
  use_git_config(user.name = "flaviadangelo", user.email = "flavia.dangelo@student.univaq.it")
+ 
+ library(gitcreds)
+ 
+
+ usethis::create_github_token()
+ 
+ # --- 5) Salva il token nel credential manager usato da Git sul tuo computer ---
+ # (quando chiede "Enter password or token:", incolla il token e premi invio)
+ gitcreds::gitcreds_set()
+ 
